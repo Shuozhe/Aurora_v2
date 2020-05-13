46c92b1f89eac832948e78be65a1e4b15ed068f7
Shuozhe Nan <shuozhe.nan@live.com>
2020-05-13 02:40:28 +0200

    added menu; added basic demo scnene from store

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+  public void StartGame()
+  {
+    Debug.Log("StartGame");
+    SceneManager.LoadScene(1);
+  }
+
+  public void ExitGame()
+  {
+    Debug.Log("ExitGame");
+    Application.Quit();
+  }
+
+  public void StartDemo()
+  {
+    SceneManager.LoadScene(2);
+  }
+
+  public void GotoMenu()
+  {
+    SceneManager.LoadScene(0);
+  }
+}
